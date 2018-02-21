112967e2ff29a6fac7917f1a259d09bc0df1a3fa
HyeSeon Yang <skcm19@naver.com>
2018-02-21 12:21:02 +0900

    공급회사, list table

--- src/main/java/software_project/panel/SupplyingCompany.java
+++ src/main/java/software_project/panel/SupplyingCompany.java
@@ -1,30 +1,36 @@
 package software_project.panel;
 
-import javax.swing.JPanel;
-import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
+
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
-
-import java.awt.FlowLayout;
-import java.awt.BorderLayout;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+
 import software_project.table.SupplyingCompanyLists;
 
 public class SupplyingCompany extends JPanel {
 	private JTextField tfSearchPC;
-	private JTextField tfPCName;
-	private JTextField tfPCPhone;
-	private JTextField tfPCZipCode;
 	private JTextField tfPCJuso;
+	private JTextField tfPCZipCode;
+	private JTextField tfPCName;
+	private JTextField tfPCNum;
 	private JTextField tfPCJusoDetail;
 	private ImageIcon img = new ImageIcon("images/twitter1.png");
+	private JTextField tfPCPhone;
+	private String [] manager = {"영업1팀","영업2팀","영업3팀","영업4팀","영업5팀"};
+	private String [] search = {"회사번호", "회사명", "관리자"};
+	ImageIcon left = new ImageIcon("images/");
 	
 	public SupplyingCompany() {
 
 		initComponents();
+		
 	}
 	private void initComponents() {
 		setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -32,7 +38,7 @@ public class SupplyingCompany extends JPanel {
 		
 		JPanel p1 = new JPanel();
 		p1.setBorder(new EmptyBorder(5, 5, 5, 5));
-		p1.setBounds(0, 0, 800, 136);
+		p1.setBounds(0, 0, 888, 136);
 		add(p1);
 		p1.setLayout(null);
 		
@@ -53,94 +59,104 @@ public class SupplyingCompany extends JPanel {
 		pImg.add(btnPCImgUp);
 		
 		JPanel pNaming = new JPanel();
-		pNaming.setBounds(126, 10, 674, 126);
+		pNaming.setBounds(126, 10, 762, 126);
 		p1.add(pNaming);
 		pNaming.setLayout(null);
 		
+		JLabel lblPCNum = new JLabel("회사번호");
+		lblPCNum.setBounds(12, 14, 61, 15);
+		pNaming.add(lblPCNum);
+		
+		tfPCNum = new JTextField();
+		tfPCNum.setBounds(76, 11, 116, 21);
+		pNaming.add(tfPCNum);
+		tfPCNum.setColumns(10);
+		
 		JLabel lblPCName = new JLabel("회사명");
-		lblPCName.setBounds(0, 14, 61, 15);
+		lblPCName.setBounds(12, 45, 57, 15);
 		pNaming.add(lblPCName);
 		
 		tfPCName = new JTextField();
-		tfPCName.setBounds(316, 42, 322, 21);
+		tfPCName.setBounds(76, 42, 116, 21);
 		pNaming.add(tfPCName);
 		tfPCName.setColumns(10);
 		
 		JLabel lblPCPhone = new JLabel("전화번호");
-		lblPCPhone.setBounds(256, 14, 61, 15);
+		lblPCPhone.setBounds(12, 76, 61, 15);
 		pNaming.add(lblPCPhone);
 		
 		tfPCPhone = new JTextField();
-		tfPCPhone.setBounds(316, 11, 150, 21);
-		pNaming.add(tfPCPhone);
 		tfPCPhone.setColumns(10);
+		tfPCPhone.setBounds(76, 73, 189, 21);
+		pNaming.add(tfPCPhone);
 		
-		JLabel lblPCManager = new JLabel("담당자");
-		lblPCManager.setBounds(490, 14, 48, 15);
+		JLabel lblPCManager = new JLabel("관리자");
+		lblPCManager.setBounds(12, 108, 48, 15);
 		pNaming.add(lblPCManager);
 		
-		JComboBox cmbPCManager = new JComboBox();
-		cmbPCManager.setBounds(538, 11, 100, 21);
+		
+		JComboBox cmbPCManager = new JComboBox(manager);
+		cmbPCManager.setBounds(77, 105, 115, 21);
 		pNaming.add(cmbPCManager);
 		
 		JLabel lblPCZipCode = new JLabel("우편번호");
-		lblPCZipCode.setBounds(0, 43, 57, 15);
+		lblPCZipCode.setBounds(330, 14, 57, 15);
 		pNaming.add(lblPCZipCode);
 		
 		tfPCZipCode = new JTextField();
-		tfPCZipCode.setBounds(60, 38, 116, 21);
+		tfPCZipCode.setBounds(403, 11, 150, 21);
 		pNaming.add(tfPCZipCode);
 		tfPCZipCode.setColumns(10);
 		
 		JButton btnPCSchZipCode = new JButton("검색");
-		btnPCSchZipCode.setBounds(181, 38, 61, 23);
+		btnPCSchZipCode.setBounds(565, 10, 61, 23);
 		pNaming.add(btnPCSchZipCode);
 		
 		JLabel lblPCJuso = new JLabel("상세주소");
-		lblPCJuso.setBounds(256, 45, 57, 15);
+		lblPCJuso.setBounds(330, 45, 57, 15);
 		pNaming.add(lblPCJuso);
 		
 		tfPCJuso = new JTextField();
-		tfPCJuso.setBounds(60, 10, 116, 21);
+		tfPCJuso.setBounds(403, 42, 347, 21);
 		pNaming.add(tfPCJuso);
 		tfPCJuso.setColumns(10);
 		
 		tfPCJusoDetail = new JTextField();
 		tfPCJusoDetail.setColumns(10);
-		tfPCJusoDetail.setBounds(316, 72, 322, 21);
+		tfPCJusoDetail.setBounds(403, 73, 347, 21);
 		pNaming.add(tfPCJusoDetail);
 		
 		JButton btnSubmitPC = new JButton("등록");
 		
-		btnSubmitPC.setBounds(502, 103, 61, 23);
+		btnSubmitPC.setBounds(616, 104, 61, 23);
 		pNaming.add(btnSubmitPC);
 		
 		JButton btnCancelPC = new JButton("취소");
-		btnCancelPC.setBounds(577, 103, 61, 23);
+		btnCancelPC.setBounds(689, 104, 61, 23);
 		pNaming.add(btnCancelPC);
 		
 		JPanel p2 = new JPanel();
 		p2.setBorder(new EmptyBorder(5, 5, 5, 5));
-		p2.setBounds(0, 146, 800, 39);
+		p2.setBounds(0, 146, 888, 39);
 		add(p2);
 		p2.setLayout(null);
 		
-		JComboBox cmbSearchPC = new JComboBox();
+		JComboBox cmbSearchPC = new JComboBox(search);
 		cmbSearchPC.setBounds(12, 10, 109, 21);
 		p2.add(cmbSearchPC);
 		
 		tfSearchPC = new JTextField();
-		tfSearchPC.setBounds(132, 10, 560, 21);
+		tfSearchPC.setBounds(132, 10, 670, 21);
 		p2.add(tfSearchPC);
 		tfSearchPC.setColumns(10);
 		
 		JButton btnSearchPC = new JButton("검색");
-		btnSearchPC.setBounds(704, 9, 60, 23);
+		btnSearchPC.setBounds(814, 9, 60, 23);
 		p2.add(btnSearchPC);
 		
 		JPanel p3 = new JPanel();
 		p3.setBorder(new EmptyBorder(5, 5, 5, 5));
-		p3.setBounds(0, 195, 800, 205);
+		p3.setBounds(0, 195, 888, 205);
 		add(p3);
 		p3.setLayout(new BorderLayout(0, 0));
 		
@@ -170,5 +186,4 @@ public class SupplyingCompany extends JPanel {
 		pPaging.add(button_4);
 
 	}
-
 }
--- src/main/java/software_project/table/SupplyingCompanyLists.java
+++ src/main/java/software_project/table/SupplyingCompanyLists.java
@@ -1,8 +1,6 @@
 package software_project.table;
 
 import java.awt.BorderLayout;
-import java.sql.SQLException;
-import java.util.List;
 
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
@@ -13,9 +11,6 @@ import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableColumnModel;
 
-
-
-
 public class SupplyingCompanyLists extends JPanel {
 	protected JTable table;
 	
@@ -23,76 +18,82 @@ public class SupplyingCompanyLists extends JPanel {
 
 		initComponents();
 	}
+
 	private void initComponents() {
 		setLayout(new BorderLayout(0, 0));
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		add(scrollPane, BorderLayout.CENTER);
-		
+
 		table = new JTable();
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		loadDatas();
 		scrollPane.setViewportView(table);
 	}
+
 	private void loadDatas() {
 		NonEditableModel model = new NonEditableModel(getRows(), getColumNames());
 		table.setModel(model);
 		setAlignWidth();
-		
+
 	}
-	
+
 	private Object[][] getRows() {
-		Object[][] rows = null;
-		/*try {
-			List<Management> list = ManagementDao.getInstance().selectItemByAll();
-			rows = new Object[list.size()][];
-			for(int i=0; i<list.size(); i++) {
-				rows[i] = list.get(i).toArray();
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}*/
-		return rows;
+		//Object[][] rows = null;
+		/*
+		 * try { List<Management> list = ManagementDao.getInstance().selectItemByAll();
+		 * rows = new Object[list.size()][]; for(int i=0; i<list.size(); i++) { rows[i]
+		 * = list.get(i).toArray(); } } catch (SQLException e) { e.printStackTrace(); }
+		 */
+		 
+		return new Object[][] {
+			{"번호", "회사명", "주소", "전화번호", "담당자", "수정", "삭제"},
+			{"번호", "회사명", "주소", "전화번호", "담당자", "수정", "삭제"},
+			{"번호", "회사명", "주소", "전화번호", "담당자", "수정", "삭제"}
+		};
 	}
+
 	public String[] getColumNames() {
-		
-		return new String[] {"번호", "회사명", "주소","전화번호","담당자"};
+
+		return new String[] { "번호", "회사명", "주소", "전화번호", "담당자", "수정", "삭제" };
 	}
+
 	private void setAlignWidth() {
-		//셀의 너비와 정렬
-				tableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3,4);
-				
-				tableCellWidth(200, 300, 500, 400, 300);
-		
+		// 셀의 너비와 정렬
+		tableCellAlign(SwingConstants.CENTER, 0, 1, 2, 3, 4, 5, 6);
+
+		tableCellWidth(200, 300, 500, 400, 300, 100, 100);
+
 	}
 
-		protected void tableCellWidth(int... width) {
-			TableColumnModel model = table.getColumnModel();
-			for (int i = 0; i < width.length; i++) {
-				model.getColumn(i).setPreferredWidth(width[i]);
-			}
-		
-	}
-		protected void tableCellAlign(int align, int... idx) {
-			DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
-			dtcr.setHorizontalAlignment(align);
-
-			TableColumnModel model = table.getColumnModel();
-			for (int i = 0; i < idx.length; i++) {
-				model.getColumn(idx[i]).setCellRenderer(dtcr);
-			}
-	}
-
-		// 테이블 내용을 수정하지 못하게 하는 클래스
-		class NonEditableModel extends DefaultTableModel {
-			public NonEditableModel(Object[][] data, Object[] columnNames) {
-				super(data, columnNames);
-			}
-
-			@Override
-			public boolean isCellEditable(int row, int column) {
-				return false;
-			}
+	protected void tableCellWidth(int... width) {
+		TableColumnModel model = table.getColumnModel();
+		for (int i = 0; i < width.length; i++) {
+			model.getColumn(i).setPreferredWidth(width[i]);
 		}
+
+	}
+
+	protected void tableCellAlign(int align, int... idx) {
+		DefaultTableCellRenderer dtcr = new DefaultTableCellRenderer();
+		dtcr.setHorizontalAlignment(align);
+
+		TableColumnModel model = table.getColumnModel();
+		for (int i = 0; i < idx.length; i++) {
+			model.getColumn(idx[i]).setCellRenderer(dtcr);
+		}
+	}
+
+	// 테이블 내용을 수정하지 못하게 하는 클래스
+	class NonEditableModel extends DefaultTableModel {
+		public NonEditableModel(Object[][] data, Object[] columnNames) {
+			super(data, columnNames);
+		}
+
+		@Override
+		public boolean isCellEditable(int row, int column) {
+			return false;
+		}
+	}
 
 }
